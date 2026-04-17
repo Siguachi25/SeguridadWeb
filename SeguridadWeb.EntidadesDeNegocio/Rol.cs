e05725af2b9e531a061c7a1033e504720c89bb0f
Siguachi25 <sp25002@esfe.agape.edu.sv>
2026-04-16 20:12:42 -0600

    Programar la entidad de Rol

--- SeguridadWeb.EntidadesDeNegocio/Rol.cs
+++ SeguridadWeb.EntidadesDeNegocio/Rol.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// *******************************************
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SeguridadWeb.EntidadesDeNegocio
 {
     public class Rol
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+        public List<Usuario> Usuarios { get; set; }
     }
 }
