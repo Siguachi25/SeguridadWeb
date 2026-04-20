ba4622f5b8dc5481d5cefa8f1e2710aeadc36d5d
Siguachi25 <sp25002@esfe.agape.edu.sv>
2026-04-20 00:10:04 -0600

    Programar el controlador de Rol

--- SeguridadWeb.UIAppWebAspCore/Controllers/RolController.cs
+++ SeguridadWeb.UIAppWebAspCore/Controllers/RolController.cs
@@ -1,82 +1,115 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/**********************************************/
+using SeguridadWeb.EntidadesDeNegocio;
+using SeguridadWeb.LogicaDeNegocio;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace SeguridadWeb.UI.AppWebAspCore.Controllers
 {
+    //[Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class RolController : Controller
     {
+        RolBL rolBL = new RolBL();
         // GET: RolController
-        public ActionResult Index()
+        public async Task<IActionResult> Index(Rol pRol = null)
         {
-            return View();
+            if (pRol == null)
+                pRol = new Rol();
+            if (pRol.Top_Aux == 0)
+                pRol.Top_Aux = 10;
+            else if (pRol.Top_Aux == -1)
+                pRol.Top_Aux = 0;
+
+            var roles = await rolBL.BuscarAsync(pRol);
+            ViewBag.Top = pRol.Top_Aux;
+            return View(roles);
         }
 
         // GET: RolController/Details/5
-        public ActionResult Details(int id)
+        public async Task<IActionResult> Details(int id)
         {
-            return View();
+            var rol = await rolBL.ObtenerPorIdAsync(new Rol { Id = id });
+            return View(rol);
         }
 
         // GET: RolController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
+            ViewBag.Error = "";
             return View();
         }
 
         // POST: RolController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create(Rol pRol)
         {
             try
             {
+                int result = await rolBL.CrearAsync(pRol);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pRol);
             }
         }
 
         // GET: RolController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<IActionResult> Edit(Rol pRol)
         {
-            return View();
+            var rol = await rolBL.ObtenerPorIdAsync(pRol);
+            ViewBag.Error = "";
+            return View(rol);
         }
 
         // POST: RolController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, Rol pRol)
         {
             try
             {
+                int result = await rolBL.ModificarAsync(pRol);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pRol);
             }
         }
 
         // GET: RolController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<IActionResult> Delete(Rol pRol)
         {
-            return View();
+            ViewBag.Error = "";
+            var rol = await rolBL.ObtenerPorIdAsync(pRol);
+            return View(rol);
         }
 
         // POST: RolController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> Delete(int id, Rol pRol)
         {
             try
             {
+                int result = await rolBL.EliminarAsync(pRol);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex.Message;
+                return View(pRol);
             }
         }
     }
